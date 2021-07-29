efb1fa3cfee10b4a36727d8f3adf838cc8378b59
Bryan Galvin <bcgalvin@gmail.com>
2021-07-28 18:59:08 -0700

    +missing projen updates

--- .projenrc.js
+++ .projenrc.js
@@ -1,19 +1,119 @@
-const { AwsCdkConstructLibrary } = require('projen');
-const project = new AwsCdkConstructLibrary({
-  author: 'Bryan Galvin',
-  authorAddress: 'bcgalvin@gmail.com',
-  cdkVersion: '1.95.2',
-  defaultReleaseBranch: 'main',
-  name: 'metaflow-cdk',
-  repositoryUrl: 'git@github.com:bcgalvin/metaflow-cdk.git',
+const {
+  AwsCdkConstructLibrary,
+  ProjectType,
+  NodePackageManager,
+  DependenciesUpgradeMechanism,
+} = require('projen');
 
-  // cdkDependencies: undefined,        /* Which AWS CDK modules (those that start with "@aws-cdk/") does this library require when consumed? */
-  // cdkTestDependencies: undefined,    /* AWS CDK modules required for testing. */
-  // deps: [],                          /* Runtime dependencies of this module. */
-  // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],                       /* Build dependencies for this module. */
-  // packageName: undefined,            /* The "name" in package.json. */
-  // projectType: ProjectType.UNKNOWN,  /* Which type of project this is (library/app). */
-  // release: undefined,                /* Add release management to this project. */
+const cdkVersion = '1.116.0';
+const deps = [];
+const devDeps = [
+  'ts-node',
+  'constructs',
+];
+const cdkDependencies = [
+  '@aws-cdk/core',
+  '@aws-cdk/aws-ec2',
+];
+const peerDependencies = [
+  'constructs',
+];
+const bundledDependencies = [];
+const common_exclude = [
+  'cdk.out',
+  'cdk.context.json',
+  'yarn-error.log',
+  '.vscode',
+  '.DS_Store',
+  '.idea',
+];
+const tsCustomConfig = {
+  exclude: ['doc'],
+  include: ['src/'],
+};
+
+const project = new AwsCdkConstructLibrary({
+  name: 'metaflow-cdk',
+  repositoryUrl: 'https://github.com/bcgalvin/metaflow-cdk',
+  description: 'A JSII construct library to build Metaflow infrastructure on AWS using Python, Typescript or Go.',
+  author: 'bcgalvin',
+  authorAddress: 'bcgalvin@gmail.com',
+  license: 'Apache-2.0',
+  stability: 'experimental',
+  projectType: ProjectType.LIB,
+  packageManager: NodePackageManager.YARN,
+  keywords: ['aws-cdk', 'cdk-construct', 'metaflow'],
+  // repo
+  defaultReleaseBranch: 'main',
+  mergifyAutoMergeLabel: 'dependencies',
+  pullRequestTemplateContents: [
+    '---',
+    'By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.',
+  ],
+  depsUpgrade: DependenciesUpgradeMechanism.githubWorkflow({
+    workflowOptions: {
+      labels: ['auto-approve', 'auto-merge'],
+      secret: 'PROJEN_GITHUB_TOKEN',
+    },
+  }),
+  autoApproveOptions: {
+    secret: 'GITHUB_TOKEN',
+  },
+  gitpod: true,
+  // dependencies
+  cdkVersion: cdkVersion,
+  cdkVersionPinning: true,
+  cdkDependenciesAsDeps: false,
+  deps: deps,
+  devDeps: devDeps,
+  cdkDependencies: cdkDependencies,
+  bundledDeps: bundledDependencies,
+  peerDependencyOptions: {
+    pinnedDevDependency: false,
+  },
+  peerDependencies: peerDependencies,
+  minNodeVersion: '12.13.0',
+  jestOptions: {
+    typescriptConfig: {
+      compilerOptions: {
+        esModuleInterop: true,
+      },
+    },
+  },
+  // devtools
+  codeCov: true,
+  // typescript
+  tsconfig: tsCustomConfig,
+  // release
+  releaseEveryCommit: false,
+  prerelease: 'beta',
+  releaseBranchesreleaseBranches: ['main'],
+  releaseToNpm: true,
+  publishToPypi: {
+    distName: 'metaflow-cdk',
+    module: 'metaflow_cdk',
+  },
 });
+// disable go sumdb so that go deps are resolved directly against github
+project.tasks.tryFind('package').prependExec('go env -w GOSUMDB=off');
+project.gitignore.exclude(...common_exclude);
+const eslintConfig = project.tryFindObjectFile('.eslintrc.json');
+eslintConfig.addOverride('rules', {
+  '@typescript-eslint/no-require-imports': 'off',
+  '@typescript-eslint/no-var-requires': 'off',
+  'brace-style': 'off',
+});
+const tsconfig = project.tryFindObjectFile('tsconfig.eslint.json');
+tsconfig.addOverride('include', [
+  '.projenrc.js',
+  'src/**/*.ts',
+  'test/**/*.ts',
+]);
+project.gitpod.addTasks({
+  name: 'Setup',
+  init: 'yarn install',
+  command: 'npx projen build',
+});
+const openCoverage = project.addTask('coverage');
+openCoverage.exec('npx projen test && open coverage/lcov-report/index.html');
 project.synth();
